8db4a22df0df02f807a082e2cc1424d61941e1d2
gitjnichols3 <j_nichols2@mail.fhsu.edu>
2025-12-02 15:55:32 -0700

    Added photo schema.

---
+++ backend/src/models/Photo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const photoSchema = new mongoose.Schema(
+  {
+    albumId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Album',
+      required: true,
+    },
+    eventId: {
+      type: String, // matches eventId in Album.events
+      required: false, // you might allow "unassigned" photos later
+    },
+    photographerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    originalUrl: {
+      type: String, // or "path" if you prefer
+      required: true,
+    },
+    thumbnailUrl: {
+      type: String,
+      required: false, // you can add this later when you implement Sharp
+    },
+    takenAt: {
+      type: Date,
+      required: false, // will come from EXIF later; can fallback to uploadedAt
+    },
+    uploadedAt: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    // tags, rotation, etc. can go here later
+  },
+  {
+    timestamps: true, // createdAt, updatedAt
+  }
+);
+
+const Photo = mongoose.model('Photo', photoSchema);
+
+module.exports = Photo;
